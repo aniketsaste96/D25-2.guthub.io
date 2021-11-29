55bdce70f470f170b0bdd75c20beab4d62ad8817
Aniket <aniketsaste96@gmail.com>
2021-11-29 12:40:58 +0530

    D25-2

---
+++ script.js
@@ -0,0 +1,42 @@
+
+//Get hold of Elements
+var maindiv = document.querySelector(".container").innerHTML
+const subdiv = document.getElementsByClassName("subcontainer")
+const btn = document.querySelector(".btn")
+
+//api Url 
+const url = "https://dog.ceo/api/breeds/image/random"
+
+
+
+//Fetch data on event
+btn.addEventListener("click", () => {
+
+    const fetchData = async () => {
+        try {
+
+
+            const response = await fetch(url);
+            const Qdata = await response.json();
+
+
+            //appending data to HTML
+            let realData = Qdata
+            document.getElementById("img").innerHTML = ` <img src="${realData.message}"></img> `
+
+
+            //Error Handling
+        } catch (err) {
+            console.log(err)
+        }
+    };
+    //calling fecth function
+    fetchData();
+
+});
+
+
+
+
+
+
